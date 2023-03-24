bd8785d348e299cdd489cadfe4d99becd01f15e6
GeorgeKlemperer <george@klemperer.co.uk>
2023-03-24 18:55:13 +0000

    button added per movie

--- my-scripts.js
+++ my-scripts.js
@@ -54,125 +54,114 @@ let movieData = {
 };
 
 function updateMovieCount() {
-  const movieCountElement = document.getElementById('movieCount');
+  const movieCountElement = document.getElementById("movieCount");
   const count = Object.keys(movieData).length;
   movieCountElement.innerHTML = `The only ${count} movies you need to see (and counting).`;
 }
 
 // this code adds form values to movieData object then reprints object.
-function addmoviedict () {
-
+function addmoviedict() {
   //This code prevents interger object keys breaking sorting code, by forcing qoute marks around numbers.
-  if (isNaN(moviename.value)==false) {
-    mname = isNaN(moviename.value) ? key : `"${moviename.value}"`; 
-    console.log("returned false")
-    console.log(mname)
-  }
-  else {
+  if (isNaN(moviename.value) == false) {
+    mname = isNaN(moviename.value) ? key : `"${moviename.value}"`;
+    console.log("returned false");
+    console.log(mname);
+  } else {
     mname = moviename.value;
-    console.log("returned true")
-    console.log(mname)
+    console.log("returned true");
+    console.log(mname);
   }
 
-  
-  // mname = isNaN(moviename.value) ? key : `"${moviename.value}"`; 
+  // mname = isNaN(moviename.value) ? key : `"${moviename.value}"`;
 
-
-  myear =  movieyear.value
-  mrating = movierating.value
-  mruntime = movieruntime.value
-  mplot = movieplot.value
-  mcast = moviecast.value//.split(',').map(castMember => castMember.trim()); //This turns value into an array
+  myear = movieyear.value;
+  mrating = movierating.value;
+  mruntime = movieruntime.value;
+  mplot = movieplot.value;
+  mcast = moviecast.value; //.split(',').map(castMember => castMember.trim()); //This turns value into an array
 
   let tempmoviedict = {
-  // a = moviename.value
-  year: myear,
-  rating: mrating,
-  runtime: mruntime,
-  plot: mplot,
-  cast: mcast
-  }
+    // a = moviename.value
+    year: myear,
+    rating: mrating,
+    runtime: mruntime,
+    plot: mplot,
+    cast: mcast,
+  };
 
-// the || means "or" and ! means blank
-  if (!mname||!myear||!mrating||!mruntime||!mplot||!mcast) { 
+  // the || means "or" and ! means blank
+  if (!mname || !myear || !mrating || !mruntime || !mplot || !mcast) {
     alert("Please fill in all fields! :)");
-    }
-    else
-    {
-  movieData[mname] = tempmoviedict
+  } else {
+    movieData[mname] = tempmoviedict;
 
-  moviecard(movieData)
+    moviecard(movieData);
 
-  moviename.value = ""
-movieyear.value = ""
-movierating.value = ""
-movieruntime.value = ""
-movieplot.value = ""
-moviecast.value = ""
-console.log("all fine")
-    }
-// above code resets all fields so they are empty again
-}  
+    moviename.value = "";
+    movieyear.value = "";
+    movierating.value = "";
+    movieruntime.value = "";
+    movieplot.value = "";
+    moviecast.value = "";
+    console.log("all fine");
+  }
+  // above code resets all fields so they are empty again
+}
 
 // Old add movie function (simply printed out form values)
-function addmovie () {
+function addmovie() {
+  a = moviename.value;
+  b = movieyear.value;
+  c = movierating.value;
+  d = movieruntime.value;
+  e = movieplot.value;
+  f = moviecast.value;
 
-a = moviename.value
-b = movieyear.value
-c = movierating.value
-d = movieruntime.value
-e = movieplot.value
-f = moviecast.value;
+  // the || means "or" and ! means blank
+  if (!a || !b || !c || !d || !e || !f) {
+    alert("Please fill in all fields! :)");
+  } else {
+    const p = document.createElement("p");
+    const br = document.createElement("br");
+    const u = document.createElement("u");
+    const div = document.createElement("div");
 
+    // let input = document.getElementById("moviename");
+    // movieData[input.value]
 
-// the || means "or" and ! means blank
-if (!a||!b||!c||!d||!e||!f) { 
-alert("Please fill in all fields! :)");
+    var cardtext = `<u>${a}</u><br>Year: ${b}<br> Rating: ${c}<br>Runtime: ${d}<br> Plot: ${e}<br> Cast: ${f}`;
+
+    p.innerHTML = cardtext;
+    div.appendChild(p);
+
+    document.getElementById("flex-container").appendChild(div);
+  }
+  // moviecard(movieData) // This function is called again so cards are re-made with new object key added
+
+  moviename.value = "";
+  movieyear.value = "";
+  movierating.value = "";
+  movieruntime.value = "";
+  movieplot.value = "";
+  moviecast.value = "";
+  // above code resets all fields so they are empty again
 }
-else
-{
-const p = document.createElement("p");
-const br = document.createElement("br");
-const u = document.createElement("u");
-const div = document.createElement("div");
-
-// let input = document.getElementById("moviename");
-// movieData[input.value]
-
-var cardtext = `<u>${a}</u><br>Year: ${b}<br> Rating: ${c}<br>Runtime: ${d}<br> Plot: ${e}<br> Cast: ${f}`
-
-p.innerHTML = cardtext
-div.appendChild(p);
-
-document.getElementById("flex-container").appendChild(div);
-}
-// moviecard(movieData) // This function is called again so cards are re-made with new object key added
-
-moviename.value = ""
-movieyear.value = ""
-movierating.value = ""
-movieruntime.value = ""
-movieplot.value = ""
-moviecast.value = ""
-// above code resets all fields so they are empty again
-}
-
 
 function rearrangemovies(year) {
   container_element.innerHTML = "";
 
   // Convert movieData object into an array of its values
-  const movieArray = Object.keys(movieData).map(key => {
+  const movieArray = Object.keys(movieData).map((key) => {
     return { ...movieData[key], title: key };
   });
 
-  console.log("Original array")
-  console.log(movieArray)
+  console.log("Original array");
+  console.log(movieArray);
 
-    // Sort the array based on the year property in ascending order
+  // Sort the array based on the year property in ascending order
   const sortedMovieArray = movieArray.sort((p1, p2) =>
-  p1.year < p2.year ? -1 : p1.year > p2.year ? 1 : 0
-);
+    p1.year < p2.year ? -1 : p1.year > p2.year ? 1 : 0
+  );
 
   // Create a new sorted movie object
   const sortedMovieData = sortedMovieArray.reduce((obj, item) => {
@@ -180,82 +169,69 @@ function rearrangemovies(year) {
     return obj;
   }, {});
 
-  console.log("Sorted log")
-  console.log(sortedMovieArray)
+  console.log("Sorted log");
+  console.log(sortedMovieArray);
 
   moviecard(sortedMovieData);
 
-  movieData=sortedMovieData
+  movieData = sortedMovieData;
 }
-
-
 
 // This function prints out Object input
-function moviecard (movieinfo) {
+function moviecard(movieinfo) {
+  container_element = document.getElementById("flex-container");
 
-container_element = document.getElementById("flex-container")
+  // This code sets container element content to equal nothing (which basically resets the list)
+  container_element.innerHTML = "";
 
-// This code sets container element content to equal nothing (which basically resets the list)
-container_element.innerHTML = "";
+  // Gives all movies a title property
+  for (const key of Object.keys(movieData)) {
+    movieData[key].title = key;
 
-// Gives all movies a title property
-for (const key of Object.keys(movieData)) {
-  movieData[key].title = key;
-
-  console.log("Key")
-  console.log(key)
-
-}
-
-
-
-for (key of Object.keys(movieinfo)) {
-  console.log(key)
-  const name = key
-  const year = movieinfo[key].year // key here is in square brackets as there is spaces in it.
-  const rating = movieinfo[key].rating
-  const runtime = movieinfo[key].runtime
-  const plot = movieinfo[key].plot
-
-
-  let cast;
-  if (Array.isArray(movieinfo[key].cast)) {
-    cast = movieinfo[key].cast.join(", ");
-  } else {
-    cast = movieinfo[key].cast;
+    console.log("Key");
+    console.log(key);
   }
 
-// above if statement formats cast to have spaces if data type is an object (such as movieData list), else it will just print as string.
+  for (key of Object.keys(movieinfo)) {
+    console.log(key);
+    const name = key;
+    const year = movieinfo[key].year; // key here is in square brackets as there is spaces in it.
+    const rating = movieinfo[key].rating;
+    const runtime = movieinfo[key].runtime;
+    const plot = movieinfo[key].plot;
 
-  const p = document.createElement("p");
-  const br = document.createElement("br");
-  const u = document.createElement("u");
-  const div = document.createElement("div");
+    let cast;
+    if (Array.isArray(movieinfo[key].cast)) {
+      cast = movieinfo[key].cast.join(", ");
+    } else {
+      cast = movieinfo[key].cast;
+    }
 
-  var cardtext = `<u>${key}</u><br>Year: ${year}<br> Rating: ${rating}<br>Runtime: ${runtime}<br> Plot: ${plot}<br> Cast: ${cast}.`
+    // above if statement formats cast to have spaces if data type is an object (such as movieData list), else it will just print as string.
 
-  p.innerHTML = cardtext
+    const p = document.createElement("p");
+    const br = document.createElement("br");
+    const u = document.createElement("u");
+    const div = document.createElement("div");
+    const button = document.createElement("button");
 
-  div.appendChild(p);
+    var cardtext = `<u>${key}</u><br>Year: ${year}<br> Rating: ${rating}<br>Runtime: ${runtime}<br> Plot: ${plot}<br> Cast: ${cast}.`;
 
+    p.innerHTML = cardtext;
 
+    div.appendChild(p);
+    div.appendChild(button);
 
-  container_element.appendChild(div);
+    container_element.appendChild(div);
 
-  // console.log(name,year,rating,runtime,plot,cast)
+    // console.log(name,year,rating,runtime,plot,cast)
 
-   // Update the movie count
-   updateMovieCount();
-
+    // Update the movie count
+    updateMovieCount();
+  }
 }
-}
 
-
-
-
-
-moviecard(movieData)
-
+moviecard(movieData);
 
 // // Creates <p> element element:
 // const movie1info = document.createElement("p");
@@ -264,17 +240,12 @@ moviecard(movieData)
 // // Append "para" to another element:
 // document.getElementById("movie1").appendChild(movie1info);
 
-
-
 // // Creates <p> element element:
 // const movie2info = document.createElement("p");
 // // Adds text to <p element:
 // movie2info.innerHTML = "<u>The Royal Tenenbaums</u><br>Year: " + movieData["The Royal Tenenbaums"].year + "<br>Rating: " + movieData["The Royal Tenenbaums"].rating + "<br>Runtime: " + movieData["The Royal Tenenbaums"].runtime + "<br>Plot:<br>" + movieData["The Royal Tenenbaums"].plot + "<br>Cast:<br>" + movieData["The Royal Tenenbaums"].cast.join(", ");
 // // Append "para" to another element:
 // document.getElementById("movie2").appendChild(movie2info);
-
-
-
 
 // // Creates <p> element element:
 // const movie3info = document.createElement("p");
@@ -283,13 +254,9 @@ moviecard(movieData)
 // // Append "para" to another element:
 // document.getElementById("movie3").appendChild(movie3info);
 
-
-
 // // Creates <p> element element:
 // const movie4info = document.createElement("p");
 // // Adds text to <p element:
 // movie4info.innerHTML = "<u>The Grand Budapest Hotel</u><br>Year: " + movieData["The Grand Budapest Hotel"].year + "<br>Rating: " + movieData["The Grand Budapest Hotel"].rating + "<br>Runtime: " + movieData["The Grand Budapest Hotel"].runtime + "<br>Plot:<br>" + movieData["The Grand Budapest Hotel"].plot + "<br>Cast:<br>" + movieData["The Grand Budapest Hotel"].cast.join(", ");
 // // Append "para" to another element:
 // document.getElementById("movie4").appendChild(movie4info);
-
-
