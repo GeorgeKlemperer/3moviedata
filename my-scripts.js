e6dbb62e2438d1b2aa41f29b0e34e1f75bab9873
GeorgeKlemperer <george@klemperer.co.uk>
2023-03-25 22:02:15 +0000

    typo fix

--- my-scripts.js
+++ my-scripts.js
@@ -34,7 +34,7 @@ let movieData = {
   },
   "Fantastic Mr. Fox": {
     year: 2009,
-    plot: "An urbane fox cannot resist returning to his farm raiding ways and then must help his community survive the farmers.' retaliation.",
+    plot: "An urbane fox cannot resist returning to his farm raiding ways and then must help his community survive the farmers' retaliation.",
     cast: [
       "George Clooney",
       "Meryl Streep",
